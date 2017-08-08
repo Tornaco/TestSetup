6492cdf0636b06c02fbb6b2ef5c55ecd46b6e542
tornaco <tornaco@163.com>
2017-08-08 11:05:57 +0800

    Add method to grant all

--- setuper/src/main/java/com/android/setuper/TestSetuper.java
+++ setuper/src/main/java/com/android/setuper/TestSetuper.java
@@ -1,11 +1,16 @@
 package com.android.setuper;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.uiautomator.UiDevice;
 
 import junit.framework.Assert;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by guohao4 on 2017/8/4.
@@ -25,6 +30,39 @@ public abstract class TestSetuper {
         String res = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
                 .executeShellCommand(cmd);
         Assert.assertTrue(res == null || !res.contains("Err"));
+    }
+
+    public static void grantAllDeclearedPermissions(String packageName) throws IOException {
+        String[] permissions = getPkgInfo(InstrumentationRegistry.getTargetContext(), packageName).requestedPermissions;
+        permissions = extractUnGranted(InstrumentationRegistry.getTargetContext(), packageName, permissions);
+        for (String p : permissions) {
+            grantPermissions(packageName, p);
+        }
+    }
+
+    private static String[] extractUnGranted(Context context, String packageName, String[] declaredPerms) {
+        if (declaredPerms == null || declaredPerms.length == 0) return null;
+        PackageManager packageManager = context.getPackageManager();
+        List<String> requestList = new ArrayList<>(declaredPerms.length);
+        for (String info : declaredPerms) {
+            int code = packageManager.checkPermission(info, packageName);
+            if (code == PackageManager.PERMISSION_GRANTED) continue;
+            requestList.add(info);
+        }
+        String[] out = new String[requestList.size()];
+        for (int i = 0; i < requestList.size(); i++) {
+            out[i] = requestList.get(i);
+        }
+        return out;
+    }
+
+    private static PackageInfo getPkgInfo(Context context, String packageName) {
+        PackageManager packageManager = context.getPackageManager();
+        try {
+            return packageManager.getPackageInfo(packageName, PackageManager.GET_PERMISSIONS);
+        } catch (PackageManager.NameNotFoundException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     public static void setStayAwake() throws IOException {
